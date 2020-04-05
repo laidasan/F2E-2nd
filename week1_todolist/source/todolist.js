8fbf6c4772b65fc139f94b9a26eda6c2219e5fa9
laidasan <tp6m4xup6tp6@gmail.com>
2020-04-06 00:55:36 +0800

    finish task star. next mission: complete add task feature

--- week1_todolist/source/todolist.js
+++ week1_todolist/source/todolist.js
@@ -1,13 +1,71 @@
 (() => {
     const $addTaskBtn = document.querySelector('.feature-add')
     const $todolist = document.querySelector('.todolist');
-    const $todolistTasks = document.querySelectorAll('.todolist__task')
-    const $todolistTaskTags = document.querySelectorAll('.todolist__task__tag')
+    let $todolistTasks = document.querySelectorAll('.todolist__task')
+    // const $todolistTaskTags = document.querySelectorAll('.todolist__task__tag')
     const $todolistFormWrap = document.querySelectorAll('.todolist__task__formWrap')
     const edit = 'todolist__task--edit'
 
+    let $taskHtml = `
+    <div class="todolist__task__tag" data-index="1">
+                <h4 class="todolist__task__title heading-fourth">
+                    <input type="checkbox" name="TaskCheckBox" class="todolist__task__checkbox">
+                    <div class="todolist__task__tagFeature">
+                        <i class="far fa-star todolist__task__tagFeatureStar star" data-index="1"></i>
+                        <i class="fas fa-pen todolist__task__tagFeatureEdit edit" data-index="1"></i>
+                    </div>
+                    <span class="todolist__task__titleContent">title</span>
+                    <input type="text" name="Title" placeholder="task title" class="todolist__task__titleInput">
+                </h4>
+                <div class="todolist__task__tagDescription">
+                    <i class="far fa-calendar-alt"></i><span>4/1</span>
+                </div>
+            </div>
+            <form action="" class="todolist__task__form">
+                <div class="todolist__task__formWrap deadline" data-type="Deadline">
+                    <h5 class="todolist__task__formHeading heading-fifth">
+                        <i class="far fa-calendar-alt"></i>
+                        Deadline
+                    </h5>
+                    <div class="todolist__task__formGroup todolist__task__formGroup--2-1">
+                        <input type="date" name="DeadlineDay" class="todolist__task__formDay" >
+                    </div>
+                    <div class="todolist__task__formGroup todolist__task__formGroup--2-1">
+                        <a href="#" class="todolist__task__formTime">hh/mm</a>
+                    </div>
+                </div>
+                <div class="todolist__task__formWrap file">
+                    <h5 class="todolist__task__formHeading heading-fifth">
+                        <i class="far fa-file"></i>
+                        File
+                    </h5>
+                    <div class="todolist__task__formGroup todolist__task__formGroup--2-1">
+                        <span class="todolist__task__form__fileInfo todolist__task__form__fileName"></span>
+                        <span class="todolist__task__form__fileInfo todolist__task__form__fileTime"></span>
+                    </div>
+                    <!--  for="UploadFile"  -->
+                    <label class="todolist__task__formLabel todolist__task__formLabel--upload">
+                        <input type="file" name="UploadFile" class="todolist__task__formUpload">
+                        <i class="fas fa-plus"></i>
+                    </label>
+                </div>
+                <div class="todolist__task__formWrap description">
+                    <h5 class="todolist__task__formHeading heading-fifth">
+                        <i class="far fa-comment-dots"></i>
+                        description
+                    </h5>
+                    <!-- <input type="text"> -->
+                    <textarea name="TaskDescription" rows="9" class="todolist__task__formDescription"></textarea>
+                    <span class="todolist__task__formDescriptionShow"></span>
+                </div>
+                <div class="todolist__task__formGroup">
+                    <button class="todolist__task__formButton todolist__task__formButton--cancel" value="cancel" type="reset" data-index="1">Cancel</button>
+                    <button class="todolist__task__formButton todolist__task__formButton--save" value="save" type="submit" data-index="1">Save</button>
+                </div>
+            </form>`
 
 
+    
     //task show description and file
     function show($task,$wraps) {
         console.log('task show')
@@ -30,11 +88,39 @@
         $task.dataset['isshow'] = 0
     }
 
+    //wrap transitionend show
+    function formWrapShow(e) {
+        let wrap = e.target
+        let $task = wrap.parentElement.parentElement
+        let isediting = parseInt($task.dataset['isedit'])
+        let istaskshow = parseInt($task.dataset['isshow'])
+        console.log(wrap.querySelector('span').textContent)
+        istaskshow && !isediting ? wrap.classList.add('show') : ''
+    }
+
+    function enterAddTask(target,currentTarget) {
+        let $taskAdd = target.nextElementSibling
+        $taskAdd.style.setProperty('display','block')
+        target.style.setProperty('display','none')
+    }
+
+    //update task infomation
+    function updateTask(task) {
+        let taskInputs = task.querySelectorAll('input')
+        let taskTitle = task.querySelector('.todolist__task__titleContent')
+        let tagDescription = task.querySelector('.todolist__task__tagDescription')
+        let taskFileName = task.querySelector('.todolist__task__form__fileName')
+        let taskFileTime = task.querySelector('.todolist__task__form__fileTime')
+        let taskDescription = task.querySelector('.todolist__task__formDescriptionShow')
+        console.log(taskInputs)
+        console.log(taskInputs[3].files)
+    }
+
    
 
     //click -> show description and file 
     function taskShow(target,currentTarget) {
-        console.log('taskShowOrHidden',target,currentTarget)
+        console.log('taskShowOrHidden')
         // let $task = currentTarget.firstElementChild
 
         //taskTag Click 或是 task__title Click的時候可以觸發Click
@@ -57,11 +143,13 @@
 
     //enter task edit mode
     function taskEdit(target,currentTarget) {
-        console.log('taskEdit',target,currentTarget)
+        console.log('taskEdit')
         let $task = $todolistTasks[target.dataset['index']]
         const $wraps = $task.querySelectorAll('.todolist__task__formWrap')
         let isediting = parseInt($task.dataset['isedit'])
-        isediting ? escEdit() : enterEdit()
+
+        //如果index是0 => addTask的task and form
+        parseInt(target.dataset['index']) ? isediting ? escEdit() : enterEdit() : escAddTask()
 
         function enterEdit() {
             console.log('enter edit')
@@ -75,15 +163,65 @@
             $task.dataset['isedit'] = 0
             $task.classList.remove('edit')
         }
+
+        function escAddTask() {
+            $task.style.removeProperty('display')
+            $task.previousElementSibling.style.removeProperty('display')
+            console.log($task.previousElementSibling)
+        }
         
     }
 
     //change task status(finish or not)
     function taskStatus(target,currentTarget) {
-        console.log('taskStatus',target,currentTarget)
+        console.log('taskStatus')
         let $task = target.parentElement.parentElement.parentElement
-        console.log($task)
         target.checked ? $task.classList.add('complete') : $task.classList.remove('complete')
+    }
+
+    function taskStar(target,currentTarget) {
+        let $task = $todolistTasks[target.dataset['index']]
+        if(parseInt(target.dataset['index'])) {
+            if(!$task.style.order) {
+                target.classList.add('fas')
+                target.style.setProperty('color','#f5a623')
+                $task.style.setProperty('order','-1')
+            }else {
+                target.classList.remove('fas')
+                target.style.removeProperty('color')
+                $task.style.removeProperty('order')
+
+            }
+        }
+    }
+
+    function taskAdd(target,currentTarget) {
+        console.log('task add')
+        let newTask = document.createElement('div')
+        newTask.className = 'todolist__task'
+        newTask.innerHTML = $taskHtml
+        newTask.dataset['isedit'] = 0
+        newTask.dataset['isshow'] = 0
+        newTask.querySelectorAll('.todolist__task__formWrap').forEach(wrap => {
+            if(wrap.className.includes('deadline')){return}
+            wrap.addEventListener('transitionend',e => {formWrapShow(e)})
+        })
+        let haveIndexEles = newTask.querySelectorAll('[data-index]')
+        haveIndexEles.forEach(ele => ele.dataset['index'] = $todolistTasks.length)
+        
+        //update task infomation
+        updateTask($todolistTasks[0])
+
+        currentTarget.appendChild(newTask)
+        $todolistTasks = document.querySelectorAll('.todolist__task')
+        // console.log($todolistTasks)
+        
+
+        //close task add form
+        let $task = target.parentElement.parentElement.parentElement
+        let $featureAdd = $task.previousElementSibling
+        $task.style.removeProperty('display')
+        $featureAdd.style.removeProperty('display')
     }
 
     function todolistClickHandler(e) {
@@ -101,10 +239,19 @@
             case 'status':
                 taskStatus(target,currentTarget)
                 break;
+            case 'star': 
+                taskStar(target,currentTarget)
+                break;
+            case 'add':
+                e.preventDefault()
+                console.log(e)
+                taskAdd(target,currentTarget)
+                break;
             default:
                 console.log('todolistClick')
                 break;
         }
+
 
 
         function checkActive(target) {
@@ -112,12 +259,16 @@
             if(target.matches) {
                 console.log('support matches')
                 target.matches('.edit') || target.value === 'cancel' ? active = 'edit' : active = 'show'
+                target.matches('.star') ? active = 'star' : ''
                 target.matches('.feature-add') ? active = '' : ''
             }else {
                 target.className.includes('edit') ? active = 'edit' : active = 'show'
+                target.className.includes('star') ? active = 'star' : ''
                 target.className.includes('feature-add') ? active = '' : ''
             }
             target.getAttribute('type') === 'checkbox' ? active = 'status' : ''
+            // console.log(target.getAttribute('value'))
+            target.getAttribute('value') === 'add' ? active = 'add' : ''
             // target.matches('edit') || target.className.includes('edit') ? active = 'edit' : active = 'show'
             // target.getAttribute('type') === 'checkbox' ? active = 'status' : ''
             // target.matches('feature-add') || target.className.includes('feature-add') ? active = '' : ''
@@ -126,180 +277,24 @@
     }
 
 
-    function addTask(e) {
-        let target = e.target
-        let $taskAdd = target.nextElementSibling
-        $taskAdd.style.setProperty('display','block')
-        console.log(target.nextElementSibling)
-    }
-
+ 
 
     $todolist.addEventListener('click',e => { todolistClickHandler(e) })
-    $addTaskBtn.addEventListener('click',e => addTask(e))
+    $addTaskBtn.addEventListener('click',e => enterAddTask(e.target,e.currentTarget))
 
 
     $todolistFormWrap.forEach(wrap => {
         if(wrap.dataset['type'] === 'Deadline'){return}
-        let $task = wrap.parentElement.parentElement
-        wrap.addEventListener("transitionend",e => {
-            let isediting = parseInt($task.dataset['isedit'])
-            let istaskshow = parseInt($task.dataset['isshow'])
-            console.log(istaskshow,isediting)
-            istaskshow && !isediting ? e.target.classList.add('show') : console.log('not opening')
-        })
+        wrap.addEventListener("transitionend",e => formWrapShow(e))
     })
-
-
-
-
-
-
-
-    // warzone
-
-
-
-
-    //2020/4/2
-    //用事件委派，在todolist(最頂層)addEventListener，不要在task上
-    // $todolistTasks.forEach(task => {
-    //     task.addEventListener('click',(e) => {
-    //         let target = e.target
-    //         let currentTarget = e.currentTarget
-    //         console.log(currentTarget)
-    //         let isEditActive = target.matches('.edit')
-    //         if(isEditActive) {
-    //             console.log('edit active')
-    //             let index = target.dataset['index']
-    //             let isediting = parseInt(currentTarget.dataset['isedit'])
-    //             if(isediting) {
-    //                 console.log('esc edit')
-    //                 currentTarget.dataset['isedit'] = 0
-    //                 $todolistTasks[index].classList.remove(`${edit}`)
-    //                 const wraps = currentTarget.querySelectorAll('.todolist__task__formWrap')
-    //             }else {
-    //                 console.log('enter edit')
-    //                 currentTarget.dataset['isedit'] = 1
-    //                 $todolistTasks[index].classList.add(`${edit}`)
-    //                 $todolistFormWrap.forEach(wrap => wrap.classList.remove('show'))
-    //             }
-    //         }
-    //         // console.log(target)
-    //     })
-    
-
-        //mousehover 單個效果好 但是多個task效果差
-        //因為是高度改變，會推擠next task，使用體驗不好
-        //改成click看看
-        //2020/4/2
-    //     task.addEventListener('mouseover',(e) => {
-    //         let target = e.target
-    //         let currentTarget = e.currentTarget
-    //         let isTask = target.matches('.todolist__task__tag') || target.matches('.todolist__task__form')
-            
-    //         let isediting = parseInt(currentTarget.dataset['isedit'])
-    //         console.log(currentTarget)
-    //         if(isTask && !isediting){
-    //             console.log('isTaskTag show')
-    //             isTaskTagOpen = true
-    //             const wraps = currentTarget.querySelectorAll('.todolist__task__formWrap')
-    //             for(let i = 0;i < 3;i++) {
-    //                 wraps[i].style.setProperty('position','static')
-    //             }
-    //         }else {
-    //             console.log('open fail')
-    //         }
-    
-    //     })
-    
-    //     task.addEventListener('mouseleave',(e) => {
-    //         let target = e.target
-    //         let isTask = target.matches('.todolist')
-    //         let isediting = parseInt(target.dataset['isedit'])
-    //         isTaskTagOpen = false
-    
-    //         if(isTask && !isediting) {
-    //             console.log('isTaskTag hidden')
-    //             const wraps = target.parentElement.querySelectorAll('.todolist__task__formWrap')
-    //             wraps.forEach(wrap => wrap.classList.remove('show'))
-    //         }
-    //     })
-    // })
-
-
-
-
-
-
-
-
-    // $todolist.addEventListener('click',(e) => {
-    //     let target = e.target
-    //     let currentTarget = e.currentTarget
-    //     console.log(currentTarget)
-    //     let isEditActive = target.matches('.edit')
-    //     if(isEditActive) {
-    //         console.log('edit active')
-    //         let index = target.dataset['index']
-    //         let isediting = parseInt(currentTarget.dataset['isedit'])
-    //         if(isediting) {
-    //             console.log('esc edit')
-    //             currentTarget.dataset['isedit'] = 0
-    //             $todolistTasks[index].classList.remove(`${edit}`)
-    //             const wraps = currentTarget.querySelectorAll('.todolist__task__formWrap')
-    //         }else {
-    //             console.log('enter edit')
-    //             currentTarget.dataset['isedit'] = 1
-    //             $todolistTasks[index].classList.add(`${edit}`)
-    //             $todolistFormWrap.forEach(wrap => wrap.classList.remove('show'))
-    //         }
-    //     }
-    //     // console.log(target)
-    // })
-
-    // $todolist.addEventListener('mouseover',(e) => {
-    //     let target = e.target
-    //     let currentTarget = e.currentTarget
-    //     let isTask = target.matches('.todolist__task__tag') || target.matches('.todolist__task__form')
-        
-    //     let isediting = parseInt(currentTarget.dataset['isedit'])
-    //     console.log(currentTarget)
-    //     if(isTask && !isediting){
-    //         console.log('isTaskTag show')
-    //         isTaskTagOpen = true
-    //         const wraps = currentTarget.querySelectorAll('.todolist__task__formWrap')
-    //         for(let i = 0;i < 3;i++) {
-    //             wraps[i].style.setProperty('position','static')
-    //         }
-    //     }else {
-    //         console.log('open fail')
-    //     }
-
-    // })
-
-    // $todolist.addEventListener('mouseleave',(e) => {
-    //     let target = e.target
-    //     let isTask = target.matches('.todolist')
-    //     let isediting = parseInt(target.dataset['isedit'])
-    //     isTaskTagOpen = false
-
-    //     if(isTask && !isediting) {
-    //         console.log('isTaskTag hidden')
-    //         const wraps = target.parentElement.querySelectorAll('.todolist__task__formWrap')
-    //         wraps.forEach(wrap => wrap.classList.remove('show'))
-    //     }
-    // })
-
     // $todolistFormWrap.forEach(wrap => {
     //     if(wrap.dataset['type'] === 'Deadline'){return}
     //     let $task = wrap.parentElement.parentElement
-    //     let isediting = parseInt($task.dataset['isedit'])
     //     wrap.addEventListener("transitionend",e => {
-    //         console.log('transitionend')
-    //         console.log('add show')
-    //         console.log('wrap event listener',$task)
-    //         console.log('isediting',isediting)
-    //         isTaskTagOpen && !isediting ? e.target.classList.add('show') : console.log('not opening')
+    //         let isediting = parseInt($task.dataset['isedit'])
+    //         let istaskshow = parseInt($task.dataset['isshow'])
+    //         console.log(istaskshow,isediting)
+    //         istaskshow && !isediting ? e.target.classList.add('show') : console.log('not opening')
     //     })
     // })
 })()
